628d690e3e6f4a881e4f76c7f4389edb91255c7e
Houssein Moussawi <am0149365@gmail.com>
2024-05-02 00:49:31 +0300

    created the creator middleware

---
+++ backend/middlewares/creator.middleware.js
@@ -0,0 +1,14 @@
+const USER_ROLES = require('../utils/USER_ROLES_ENUM')
+
+const creatorMiddleware = (req, res, next)=>{
+    try{
+        if( req.user.role === USER_ROLES.CREATOR)
+        return next();
+    return res.status(401).send('Unauthorized')
+    } catch (e){
+        console.log("Internal server error: ", e)
+        return res.status(500).send('Internal server error.')
+    }
+};
+
+module.exports = creatorMiddleware;
