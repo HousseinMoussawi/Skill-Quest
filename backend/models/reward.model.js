bfdf841df92c5a1aad79f27faddb892932c644ed
Houssein Moussawi <am0149365@gmail.com>
2024-05-01 21:56:38 +0300

    created the reward model

---
+++ backend/models/reward.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const REWARD_TYPES = require('../utils/REWARD_TYPES_ENUM')
+
+const rewardSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        unique: true,
+    },
+
+    image_url:{
+        type: String,
+        unique: true,
+    },
+
+    type:{
+        type: String,
+        enum :[REWARD_TYPES.BACKGROUND,REWARD_TYPES.EMOJI,REWARD_TYPES.THEME],
+        default: '',
+    },
+
+    price:{
+        type: Number,
+        default:0,
+    }
+})
+
+const Reward = mongoose.model('Reward',rewardSchema)
+
+module.exports = (Reward, rewardSchema)
