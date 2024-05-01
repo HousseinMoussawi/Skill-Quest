374d1f1866e0faab61015de7e33c70903e656387
Houssein Moussawi <am0149365@gmail.com>
2024-05-01 19:52:49 +0300

    fixed the achievements in the user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,70 +3,70 @@ const USER_ROLES = require("../utils/USER_ROLES_ENUM");
 const bcrypt = require("bcrypt");
 
 const achievementSchema = new mongoose.Schema({
-  achievement_id
+  achievement_id: mongoose.Schema.types.ObjectId,
   achievement_name: String,
   achievement_description: String,
   achievement_medal_url: String,
-})
-
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    validate: {
-      validator: function (value) {
-        return /\w+\s+\w+/.test(value);
-      },
-      message: "the full name '{VALUE}' is not a valid full name",
-    },
-  },
-
-  username: {
-    type: String,
-    require: "Username is required",
-    unique: true,
-  },
-
-  email: {
-    type: String,
-    require: "Email is required",
-    unique: true,
-  },
-  
-  password:{
-    type: String,
-    require: 'Password isrequired',
-  },
-
-  profilePictureURL:{
-    type:String,
-
-  },
-
-  role:{
-    type: String,
-    enum: [USER_ROLES.ADMIN,USER_ROLES.CREATOR,USER_ROLES.PLAYER],
-    default: USER_ROLES.PLAYER,
-  },
-
-  achievements:{
-    type:[achievementSchema]
-  }
-},
-{
-    timestamps: true,
 });
 
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          return /\w+\s+\w+/.test(value);
+        },
+        message: "the full name '{VALUE}' is not a valid full name",
+      },
+    },
+
+    username: {
+      type: String,
+      require: "Username is required",
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      require: "Email is required",
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      require: "Password isrequired",
+    },
+
+    profilePictureURL: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+      enum: [USER_ROLES.ADMIN, USER_ROLES.CREATOR, USER_ROLES.PLAYER],
+      default: USER_ROLES.PLAYER,
+    },
+
+    achievements: {
+      type: [achievementSchema],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-      this.password = await bcrypt.hash(this.password, 10);
-    }
-    return next();
-  });
-  
-  userSchema.methods.comparePassword = function (password) {
-    return bcrypt.compare(password, this.password);
-  };
-  const User = mongoose.model("User", userSchema);
-  
-  module.exports = { User, userSchema };
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  return next();
+});
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User, userSchema };
