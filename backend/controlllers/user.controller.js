d6bc9f00cb875ea8d28dad8174352724086c6d9a
Houssein Moussawi <am0149365@gmail.com>
2024-05-02 13:54:35 +0300

    created the get user by id function in the user controller

--- backend/controlllers/user.controller.js
+++ backend/controlllers/user.controller.js
@@ -1,11 +1,22 @@
 const { User } = require("../models/user.model");
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = (req, res) => {
   try {
-    const users = await User.find();
+    const users = User.find();
     return res.json(users);
   } catch (e) {
-    return res.status(500).send("Internal server error!");
+    return res.status(500).send("Internal server error!:", e);
+  }
+};
+
+const getUserById = (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = User.findById(id);
+    return res.json(user);
+  } catch (e) {
+    return res.status(500).send("Internal server error!:", e);
   }
 };
 
