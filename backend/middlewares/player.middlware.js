cce3d1c88462a6dff585ed4aebafc21acc722b21
Houssein Moussawi <am0149365@gmail.com>
2024-05-02 12:55:11 +0300

    created the skills enum in the utils file

--- backend/middlewares/player.middlware.js
+++ backend/middlewares/player.middlware.js
@@ -1,14 +1,17 @@
-const USER_ROLES = require('../utils/USER_ROLES_ENUM')
+const USER_ROLES = require("../utils/USER_ROLES_ENUM");
 
-const playerMiddleware = (req, res, next)=>{
-    try{
-        if( req.user.role === USER_ROLES.PLAYER || req.user.role === USER_ROLES.ADMIN)
-        return next();
-    return res.status(401).send('Unauthorized')
-    } catch (e){
-        console.log("Internal server error: ", e)
-        return res.status(500).send('Internal server error.')
-    }
+const playerMiddleware = (req, res, next) => {
+  try {
+    if (
+      req.user.role === USER_ROLES.PLAYER ||
+      req.user.role === USER_ROLES.ADMIN
+    )
+      return next();
+    return res.status(401).send("Unauthorized");
+  } catch (e) {
+    console.log("Internal server error: ", e);
+    return res.status(500).send("Internal server error.");
+  }
 };
 
 module.exports = playerMiddleware;
