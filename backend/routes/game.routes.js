ac8dc7ad626e65a25de1540a1fd77333e45def74
Houssein Moussawi <am0149365@gmail.com>
2024-05-04 16:09:20 +0300

    created the game routes

---
+++ backend/routes/game.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const {
+  createGame,
+  deleteGameById,
+  getAllGames,
+  updateGameById,
+  getGameById,
+} = require("../controlllers/game.controller");
+
+const adminMiddleware = require('../middlewares/admin.middleware')
+const playerMiddleware = require('../middlewares/player.middleware')
+const authMiddleware = require('../middlewares/auth.middleware')
+const creatorMiddleware = require('../middlewares/creator.middleware')
+
+const router = express.Router()
+
+router.post('/',authMiddleware, creatorMiddleware,createGame)
+router.delete('/:id',authMiddleware,creatorMiddleware,deleteGameById)
+router.get('/',authMiddleware,getAllGames)
+router.put('/:id',authMiddleware,creatorMiddleware, updateGameById)
+router.get('/:id',authMiddleware,getGameById)
