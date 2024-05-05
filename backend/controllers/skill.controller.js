4904d3453daa3a03b3472cec8f00a0fcef118252
Houssein Moussawi <am0149365@gmail.com>
2024-05-05 14:20:19 +0300

    created the update skill function by id function

--- backend/controllers/skill.controller.js
+++ backend/controllers/skill.controller.js
@@ -1,28 +1,48 @@
-const {Skill} = require('../models/skill.model')
+const { Skill } = require("../models/skill.model");
 
-const createSkill = async (req,res) => {
-    const {name} = req.body
+const createSkill = async (req, res) => {
+  const { name } = req.body;
 
-    try{
-        const createdSkill = await Skill.create({name})
+  try {
+    const createdSkill = await Skill.create({ name });
 
-        return res.status(200).json(createdSkill)
-    }catch (e) {
+    return res.status(200).json(createdSkill);
+  } catch (e) {
     return res.status(500).send("Internal server error!:", e);
   }
-}
+};
 
-const getAllSkills = async (req,res) => {
-    try{
-        const skills = await Skill.find()
+const getAllSkills = async (req, res) => {
+  try {
+    const skills = await Skill.find();
 
-        return res.status(200).json(skills)
-    }catch (e) {
+    return res.status(200).json(skills);
+  } catch (e) {
     return res.status(500).send("Internal server error!:", e);
   }
-}
+};
+
+const updateSkillById = async (req, res) => {
+  const { id } = req.params;
+
+  const { name } = req.body;
+
+  try {
+    const updatedSKill = await Skill.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedSKill)
+  } catch (e) {
+    return res.status(500).send("Internal server error!:", e);
+  }
+};
 
 module.exports = {
-    createSkill,
-    getAllSkills,
-}
+  createSkill,
+  getAllSkills,
+  updateSkillById,
+  
+};
