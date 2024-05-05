baedd9eddc9c0c7ec292523a27f19553ee03a12e
Houssein Moussawi <am0149365@gmail.com>
2024-05-05 13:15:29 +0300

    created the buy reward by id function in the reward controller

--- backend/controllers/reward.controller.js
+++ backend/controllers/reward.controller.js
@@ -61,9 +61,34 @@ const deleteRewardById = async (req, res) => {
   }
 };
 
+const buyRewardById = async (req, res) => {
+  const { userId, rewardId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+
+    const reward = await Reward.findById(rewardId);
+
+    if (user.balance > reward.price) {
+      user.userRewards.push({
+        reward_id: reward._id,
+        reward_image_url: reward.imageURL,
+        reward_name: reward.name,
+      });
+    }
+
+    await user.save();
+
+    return res.status(200).send("Reward baught successfully!");
+  } catch (e) {
+    return res.status(500).send("Internal server error!:", e);
+  }
+};
+
 module.exports = {
   createReward,
   getRewardById,
   updateRewardById,
   deleteRewardById,
+  buyRewardById,
 };
