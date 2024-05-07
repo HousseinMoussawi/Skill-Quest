fa3bf7594e9ad6d3c6440e654e019c8267cb8a9b
Houssein Moussawi <am0149365@gmail.com>
2024-05-08 02:06:12 +0300

    created the basic structure for the progress page

--- frontend/src/pages/progress/index.tsx
+++ frontend/src/pages/progress/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import './index.css'
+import { NavLink } from 'react-router-dom'
+
 
 type Props = {}
 
 const Progress = (props: Props) => {
   return (
-    <div>Progress</div>
+    <div className='whole-screen border flex center'> 
+        <div className='progress-div border'>
+          <ul className='games-list flex evenly'>
+            <li><NavLink to='asdadd'>asdasd</NavLink></li>
+            <li><NavLink to='asdadd'>asdasd</NavLink></li>
+            <li><NavLink to='asdadd'>asdasd</NavLink></li>
+            <li><NavLink to='asdadd'>asdasd</NavLink></li>
+          </ul>
+        <div className='game-container border flex'>
+            <div className='mode flex column evenly border'>
+                <ul className='center flex column'>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                </ul>
+                <ul className='center flex column'>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                </ul>
+                <ul className='center flex column'>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                </ul>
+            
+            </div>
+            <div className='levels-div border'>
+
+            </div>
+            
+        </div>
+        </div>
+    </div>
   )
 }
 
