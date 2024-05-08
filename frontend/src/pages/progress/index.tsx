fadcce24e6b12c27f706fbfffe5d860db25c8017
Houssein Moussawi <am0149365@gmail.com>
2024-05-08 13:00:48 +0300

    finished the static structure for the progress page

--- frontend/src/pages/progress/index.tsx
+++ frontend/src/pages/progress/index.tsx
@@ -8,33 +8,33 @@ type Props = {}
 const Progress = (props: Props) => {
   return (
     <div className='whole-screen flex center'> 
-        <div className='progress-div border'>
-          <ul className='games-list flex evenly'>
-            <li><NavLink to='asdadd'>asdasd</NavLink></li>
-            <li><NavLink to='asdadd'>asdasd</NavLink></li>
-            <li><NavLink to='asdadd'>asdasd</NavLink></li>
-            <li><NavLink to='asdadd'>asdasd</NavLink></li>
+        <div className='progress-div'>
+          <ul className='games-list flex between'>
+            <li><NavLink to='asdadd'>Word Invaders</NavLink></li>
+            <li><NavLink to='asdadd'>Shortcut King</NavLink></li>
+            <li><NavLink to='asdadd'>English</NavLink></li>
+            <li><NavLink to='/progress/'>French</NavLink></li>
           </ul>
-        <div className='game-container border flex'>
-            <div className='mode flex column evenly border'>
+        <div className='game-container flex'>
+            <div className='mode flex column between'>
                 <ul className='center flex column'>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='/progress/'>Beginner</NavLink></li>
+                  <li><NavLink to='asdadd'>Intermediate</NavLink></li>
+                  <li><NavLink to='asdadd'>Advanced</NavLink></li>
                 </ul>
                 <ul className='center flex column'>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>Daily challenges</NavLink></li>
+                  <li><NavLink to='asdadd'>Weekly challenges</NavLink></li>
+                  <li><NavLink to='asdadd'>Constest</NavLink></li>
                 </ul>
                 <ul className='center flex column'>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
-                  <li><NavLink to='asdadd'>asdasd</NavLink></li>
+                  <li><NavLink to='asdadd'>Infinite mode</NavLink></li>
+                  <li><NavLink to='asdadd'>Creator levels</NavLink></li>
+                  <li></li>
                 </ul>
             
             </div>
-            <div className='levels-div border'>
+            <div className='levels-div'>
 
             </div>
             
