402dcf3940e03eac289f6cdffdfa6d1bec244802
Houssein Moussawi <am0149365@gmail.com>
2024-05-09 01:50:15 +0300

    frontend: added the navbr to the games page

--- frontend/src/pages/games/index.tsx
+++ frontend/src/pages/games/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import GameCard from '../../components/game-card'
 import CreatorCard from '../../components/creator-card'
+import SearchBar from '../../components/search-bar'
+
 
 type Props = {}
 
 const Games = (props: Props) => {
   return (
     <div>
+      <SearchBar placeHolder='hello' firstFilter='emojis' secondFilter='themes' thirdFilter='backgrounds'/>
       <CreatorCard/>
       <GameCard/>
     </div>
