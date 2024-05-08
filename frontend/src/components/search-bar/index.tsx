319c76ceec525d82954aa608a68c62273204efbf
Houssein Moussawi <am0149365@gmail.com>
2024-05-09 01:48:13 +0300

    frontend: created the navbar component

---
+++ frontend/src/components/search-bar/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import './index.css'
+
+
+type Props = {
+    firstFilter:string,
+    secondFilter:string,
+    placeHolder:string,
+    thirdFilter:string,
+}
+
+const SearchBar: FC<Props> = ({firstFilter,secondFilter,thirdFilter,placeHolder}) => {
+  return (
+    <div className='search-bar flex between align-center '>
+        <div className='filters-div flex around'>
+            <h4 className='active'>All</h4>
+            <h4>{firstFilter}</h4>
+            <h4>{secondFilter}</h4>
+            <h4>{thirdFilter}</h4>
+        </div>
+        <input type="text" placeholder={placeHolder} />
+        <button>asadsad</button>
+    </div>
+  )
+}
+
+export default SearchBar
