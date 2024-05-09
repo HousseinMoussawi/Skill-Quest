f74d41d3c9dd6b00467a5cbfd4db570566f2b19d
Houssein Moussawi <am0149365@gmail.com>
2024-05-09 13:15:02 +0300

    frontend: created styling for the create page and added text for the create cards components

--- frontend/src/pages/create/index.tsx
+++ frontend/src/pages/create/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { FC } from 'react'
 import './index.css'
 import CreateCard from '../../components/create-card'
 
 
 type Props = {}
 
-const Create = (props: Props) => {
+const Create:FC<Props> = () => {
   return (
-    <div>
-      <CreateCard />
-      <CreateCard />
+    <div className='create flex align-center around relative'>
+      <CreateCard type='Game' text='Start building your own game world! Design your game, define rules, and create challenges for players.'/>
+      <CreateCard type='Level' text='Bring your ideas to life by designing exciting levels! Set objectives, add challenging goals, and make it fun for players to explore.'/>
     </div>
   )
 }
