a065cfed5d4c53efcde7b984814070f2de2ff68b
Houssein Moussawi <am0149365@gmail.com>
2024-05-09 14:50:27 +0300

    frontend: created the create cards logic in the create page

--- frontend/src/components/create-card/index.tsx
+++ frontend/src/components/create-card/index.tsx
@@ -1,14 +1,17 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import './index.css'
 
 type Props = {
     type:string,
     text:string,
+    handleCreateClick: () => void,
+    isSelected:boolean,
 }
 
-const CreateCard:FC<Props> = ({type,text}) => {
+const CreateCard:FC<Props> = ({type,text,handleCreateClick,isSelected}) => {
+
   return (
-    <div className='create-card flex between align-center column border'>
+    <div className={`create-card flex between align-center column ${isSelected? 'active' :''}`} onClick={handleCreateClick}>
         <h1>{type}</h1>
         <p>{text}</p>
         <p></p>
--- frontend/src/pages/create/index.tsx
+++ frontend/src/pages/create/index.tsx
@@ -1,15 +1,36 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import './index.css'
 import CreateCard from '../../components/create-card'
+import NextButton from '../../components/next-button'
+import { useNavigate } from 'react-router-dom'
 
 
 type Props = {}
 
-const Create:FC<Props> = () => {
+const Create: FC<Props> = () => {
+  const [selectedDiv,setSelectedDiv] = useState<string>('')
+
+  const navigate = useNavigate()
+
+  const handleDivClick = (div: string):void => {
+    setSelectedDiv(div);
+  };
+
+  const handleNextButtonClick = () => {
+    if (selectedDiv === 'game') {
+      navigate('/create-game')
+    } else if (selectedDiv === 'level') {
+      navigate('/create-level')
+    } else {
+      console.error('Please select a div before proceeding');
+    }
+  };
+
   return (
     <div className='create flex align-center around relative'>
-      <CreateCard type='Game' text='Start building your own game world! Design your game, define rules, and create challenges for players.'/>
-      <CreateCard type='Level' text='Bring your ideas to life by designing exciting levels! Set objectives, add challenging goals, and make it fun for players to explore.'/>
+      <CreateCard type='Game' text='Start building your own game world! Design your game, define rules, and create challenges for players.' handleCreateClick={() => handleDivClick('game')} isSelected={selectedDiv === 'game'}/>
+      <CreateCard type='Level' text='Bring your ideas to life by designing exciting levels! Set objectives, add challenging goals, and make it fun for players to explore.' handleCreateClick={() => handleDivClick('level')} isSelected={selectedDiv === 'level'}/>
+      <NextButton handleNextClick={handleNextButtonClick}/>
     </div>
   )
 }
