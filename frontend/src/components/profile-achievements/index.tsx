f33576d0a20cff8f5a8f89364f536381bc800248
Houssein Moussawi <am0149365@gmail.com>
2024-05-10 00:07:44 +0300

    frontend: added the profile achievements card to the profile achievements component

--- frontend/src/components/profile-achievements/index.tsx
+++ frontend/src/components/profile-achievements/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './index.css'
+import ProfileAchievementsCard from '../profile-achievement-card'
+
 
 type Props = {}
 
 const ProfileAchievements = (props: Props) => {
   return (
-    <div>ProfileAchievements</div>
+    <div className='profile-achievements flex column align center between'>
+      <h2>Achievements</h2>
+      <div className='profile-achievements-div flex column align-center'>
+        <ProfileAchievementsCard/>
+        <ProfileAchievementsCard/>
+        <ProfileAchievementsCard/>
+        <ProfileAchievementsCard/>
+      </div>
+    </div>
   )
 }
 
