91259734214267940066f7aa83cf6a04732232bc
Houssein Moussawi <am0149365@gmail.com>
2024-05-09 22:12:39 +0300

    frontend: created the profile image component structure

---
+++ frontend/src/components/profile-image/index.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react'
+import './index.css'
+
+type Props = {
+  name: string,
+  imageURL: string,
+}
+
+const ProfileImage: FC<Props> = ({name,imageURL}) => {
+  return (
+    <div className='profile-image flex column align center evenly'> 
+      <img src={imageURL} alt="" />
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+export default ProfileImage
