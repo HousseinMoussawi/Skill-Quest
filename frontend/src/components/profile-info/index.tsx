eb668446fcd5c2cd113a9d4c77130b9b0acecc27
Houssein Moussawi <am0149365@gmail.com>
2024-05-10 00:12:21 +0300

    frontend: changed the structure of the profile page

--- frontend/src/components/profile-info/index.tsx
+++ frontend/src/components/profile-info/index.tsx
@@ -1,34 +1,36 @@
-import React, { FC } from 'react'
-import './index.css'
+import React, { FC } from "react";
+import "./index.css";
 
 type Props = {
-  fullname: string,
-  email:string,
-  skills:string[],
-  favorite:string,
-}
+  fullname: string;
+  email: string;
+  skills: string[];
+  favorite: string;
+};
 
-const ProfileInfo:FC<Props> = ({email,favorite,fullname,skills}) => {
+const ProfileInfo: FC<Props> = ({ email, favorite, fullname, skills }) => {
   return (
-    <div className='profile-info'>
-      <div className='profile-info-titles flex column between'>
-          <h3>Full name</h3>
-          <h3>Email</h3>
-          <h3>Skills targeted</h3>
-          <h3>Best game</h3>
-          <button>Edit</button>
+    <div className="profile-info flex">
+      <div className="profile-info-titles flex column between">
+        <h3>Full name</h3>
+        <h3>Email</h3>
+        <h3>Skills targeted</h3>
+        <h3>Best game</h3>
+        <button>Edit</button>
       </div>
-      <div className='profile-info-values flex column between'>
-          <h3>{fullname}</h3>
-          <h3>{email}</h3>
-          <div className='profile-info-skills felx between'>{skills.map((string, index) => (
-        <h3 key={index}>{string}</h3>
-      ))}</div>
-          <h3>{favorite}</h3>
-          <h3></h3>
+      <div className="profile-info-values flex column between">
+        <h3>{fullname}</h3>
+        <h3>{email}</h3>
+        <div className="profile-info-skills flex between">
+          {skills.map((string, index) => (
+            <h3 key={index}>{string}</h3>
+          ))}
+        </div>
+        <h3>{favorite}</h3>
+        <div className="empty-div"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileInfo
+export default ProfileInfo;
--- frontend/src/pages/player-profile/index.tsx
+++ frontend/src/pages/player-profile/index.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import './index.css'
-import ProfileAchievements from '../../components/profile-achievements'
-import ProfileImage from '../../components/profile-image'
-import ProfileInfo from '../../components/profile-info'
-import PlayerGames from '../../components/player-games'
-import CreatorGames from '../../components/creator-games'
-import Image from '../../assets/login-image.png'
+import React from "react";
+import "./index.css";
+import ProfileAchievements from "../../components/profile-achievements";
+import ProfileImage from "../../components/profile-image";
+import ProfileInfo from "../../components/profile-info";
+import PlayerGames from "../../components/player-games";
+import CreatorGames from "../../components/creator-games";
+import Image from "../../assets/login-image.png";
 
-
-type Props = {}
+type Props = {};
 
 const PlayerProfile = (props: Props) => {
   return (
-    <div>
-      <ProfileImage imageURL={Image} name='Houssein'/>
-      <ProfileInfo/>
-      <ProfileAchievements/>
-      <PlayerGames/>
-      <CreatorGames/>
+    <div className="profile flex between">
+      <div className="left-div flex column between">
+        <ProfileImage imageURL={Image} name="Houssein" />
+        <ProfileAchievements />
+      </div>
+      <div className="right-div flex column between">
+        <ProfileInfo
+          email="houssein@gmail.com"
+          fullname="Houssein Moussawi"
+          favorite="Typing game"
+          skills={["typing", "english", "coding"]}
+        />
+        <PlayerGames />
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default PlayerProfile
+export default PlayerProfile;
