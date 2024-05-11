32b4275d3442c2f99d857da6202a4a0793e176e6
Houssein Moussawi <am0149365@gmail.com>
2024-05-11 11:00:11 +0300

    frontend: created the typing game configuration

--- frontend/src/pages/typing-game/index.tsx
+++ frontend/src/pages/typing-game/index.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import GameScreenCard from '../../components/game-screen-card'
-import './index.css'
-import Phaser from 'phaser'
+import React, { useEffect } from "react";
+import GameScreenCard from "../../components/game-screen-card";
+import "./index.css";
+import Phaser from "phaser";
 
-type Props = {}
+type Props = {};
 
 const TypingGame = (props: Props) => {
+  useEffect(() => {
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.WEBGL,
+      parent: "canvas",
+      width: "99%",
+      height: "99%",
+      scene:{
+        preload:preload,
+        create:create,
+        update:update,
+      },
+      physics:{
+        default:'arcade',
+        arcade:{
+            gravity:{ x: 0, y: 0 } 
+        }
+      }
+    };
 
-    
-    const config = {
-        type:Phaser.WEBGL,
-
-    }
+    new Phaser.Game(config);
+  }, []);
 
   return (
-    <div className='typing-game flex center '>
-        <GameScreenCard/>
+    <div className="typing-game flex center ">
+      <GameScreenCard />
     </div>
-  )
-}
+  );
+};
 
-export default TypingGame
+export default TypingGame;
