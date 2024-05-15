cebe7bfc01d31e717dbca338bb502f7c590aaf73
Houssein Moussawi <am0149365@gmail.com>
2024-05-15 13:49:32 +0300

    frontend: created the game profile page

--- frontend/src/pages/creators/index.tsx
+++ frontend/src/pages/creators/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import './index.css'
 import CreatorCard from '../../components/creator-card'
 import SearchBar from '../../components/search-bar'
@@ -7,9 +7,11 @@ import SearchBar from '../../components/search-bar'
 type Props = {}
 
 const Creators:FC<Props> = ({}) => {
+  const[clicked,setClicked] = useState<string>("")
+
   return (
     <div className='creators flex evenly justify-center'>
-      <SearchBar firstFilter='Official Creators' secondFilter='Normal Creators' placeHolder='Search for creators' thirdFilter='' />
+      <SearchBar firstFilter='Official Creators' secondFilter='Normal Creators' placeHolder='Search for creators' thirdFilter='' handleClickedFirstFilter={()=>setClicked('Officail Creators')} handleClickedAllFilter={()=>setClicked('All')} handleClickedThirdFilter={()=>setClicked('All')} handleClickedSecondFilter={()=>setClicked('Normal Creators')} isClicked={clicked}/>
       <CreatorCard />
       <CreatorCard />
       <CreatorCard />
--- frontend/src/pages/game/index.tsx
+++ frontend/src/pages/game/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { FC } from 'react'
+import'./index.css'
 
 type Props = {}
 
-const Game = (props: Props) => {
+const Game:FC<Props> = ({}) => {
   return (
-    <div>Game</div>
+    <div className='game flex column border align-center evenly'>
+      <div className='game-image border'>
+        <img src="http://localhost:3001/uploads/typinginvaders.png" alt="" />
+      </div>
+      <div className='game-screen-shots flex align-center'>
+        <div className='screen-shots-container flex align-center'>
+          <div className='screen-shot'>
+            <img src="http://localhost:3001/uploads/typinginvaders.png" alt="" />
+            </div>
+          <div className='screen-shot'>
+            <img src="http://localhost:3001/uploads/typinginvaders.png" alt="" />
+            </div>
+          <div className='screen-shot'>
+            <img src="http://localhost:3001/uploads/typinginvaders.png" alt="" />
+            </div>
+          <div className='screen-shot'>
+            <img src="http://localhost:3001/uploads/typinginvaders.png" alt="" />
+            </div>
+        </div>
+      </div>
+      <div className='game-description flex column evenly align-center'> 
+          <h3>this is a description of the game</h3>
+          <button>Add This Game</button>
+      </div>  
+    </div>
   )
 }
 
