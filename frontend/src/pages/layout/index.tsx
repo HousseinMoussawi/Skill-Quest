a243069ad24b64b45d33e73cec52e61bf7e9584e
Houssein Moussawi <am0149365@gmail.com>
2024-05-15 13:53:01 +0300

    frontend: adjusted the srcurity logic in the layout file

--- frontend/src/pages/layout/index.tsx
+++ frontend/src/pages/layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/navbar'
 import { useNavigate } from 'react-router-dom'
@@ -10,17 +10,30 @@ type Props = {
 
 const Layout: FC<Props> = ({isAuthorized}) => {
   const navigate = useNavigate()
+  const [user,setUser] = useState<any>()
+  const [username,setUsername] = useState<string>('')
+  const [userImageURL,setUserImageURL] = useState<string>('')
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if (!isAuthorized && !token) {
+    const userr = localStorage.getItem('user')
+    setUser(JSON.parse(localStorage.getItem("user") as string))
+    console.log(user)
+    if ((!isAuthorized && !token ) ||  !userr) {
       navigate('/auth');
     }
   }, [isAuthorized, navigate]);
 
+  useEffect(()=>{
+
+    if(user)
+      setUsername(user.username)
+  },[user])
+
   return (
     <div className='flex center column'>
-        <Navbar name="Houssein" imageURL="aadqqdaw"/>
+        <Navbar name={
+          username} imageURL="http://localhost:3001/uploads/images.jpg"/>
         <Outlet/>
     </div>
   )
