0755ecc56aab1ce49225a82d323a618f7bf43449
Houssein Moussawi <am0149365@gmail.com>
2024-05-18 16:11:50 +0300

    frontend: fixed the logic of the creators page

--- frontend/src/pages/creators/index.tsx
+++ frontend/src/pages/creators/index.tsx
@@ -1,27 +1,61 @@
-import React, { FC, useState } from 'react'
-import './index.css'
-import CreatorCard from '../../components/creator-card'
-import SearchBar from '../../components/search-bar'
+import React, { FC, useEffect, useState } from "react";
+import "./index.css";
+import CreatorCard from "../../components/creator-card";
+import SearchBar from "../../components/search-bar";
+import axios from "axios";
 
+type Props = {};
 
-type Props = {}
+const Creators: FC<Props> = ({}) => {
+  const [clicked, setClicked] = useState<string>("");
+  const [creators, setCreators] = useState<any[]>([]);
+  const token = localStorage.getItem("token");
 
-const Creators:FC<Props> = ({}) => {
-  const[clicked,setClicked] = useState<string>("")
+  const getAllCreators = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/users/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = response.data;
+      setCreators(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCreators();
+  }, []);
 
   return (
-    <div className='creators flex evenly justify-center'>
-      <SearchBar firstFilter='Official Creators' secondFilter='Normal Creators' placeHolder='Search for creators' thirdFilter='' handleClickedFirstFilter={()=>setClicked('Officail Creators')} handleClickedAllFilter={()=>setClicked('All')} handleClickedThirdFilter={()=>setClicked('All')} handleClickedSecondFilter={()=>setClicked('Normal Creators')} isClicked={clicked}/>
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
-      <CreatorCard />
+    <div className="creators flex evenly justify-center">
+      <SearchBar
+        firstFilter="Official Creators"
+        secondFilter="Normal Creators"
+        placeHolder="Search for creators"
+        thirdFilter=""
+        handleClickedFirstFilter={() => setClicked("Officail Creators")}
+        handleClickedAllFilter={() => setClicked("All")}
+        handleClickedThirdFilter={() => setClicked("All")}
+        handleClickedSecondFilter={() => setClicked("Normal Creators")}
+        isClicked={clicked}
+      />
+      {creators.map((creator) => {
+        if (creator.role === "CREATOR") {
+          return (
+            <CreatorCard
+              creatorImageURL={creator.profilePictureURL}
+              creatorName={creator.username}
+            />
+          );
+        }
+        return null;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Creators
+export default Creators;
