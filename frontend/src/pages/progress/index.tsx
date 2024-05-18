9959f57d4d078746b708e90d385b908018dc174f
Houssein Moussawi <am0149365@gmail.com>
2024-05-18 16:21:42 +0300

    frontend: created the logic of the progress page

--- frontend/src/pages/progress/index.tsx
+++ frontend/src/pages/progress/index.tsx
@@ -1,66 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 import { NavLink } from 'react-router-dom'
 import LevelCard from '../../components/level-card'
+import axios from 'axios'
 
 
 
 type Props = {}
 
+
 const Progress = (props: Props) => {
+
+  const [games,setGames] = useState<any[]>([])
+  const [selectedGame,setSelectedGame] = useState<any>()
+  const [gameLevels,setGameLevels] = useState<any[]>([])
+  const [clickedMode,setCLickedMode] = useState<string>('')
+  const [clickedGame,setClickedGame] = useState<string>('')
+  const [gameName,setGameName] = useState<string>('')
+
+
+  const token = localStorage.getItem('token')
+  const user = JSON.parse(localStorage.getItem('user') as string)
+
+  const getUserGames = async()=>{
+    try {
+      const response = await axios.get(`http://localhost:3001/users/games/${user._id}`,
+        {
+          headers:{
+            Authorization:`Bearer ${token}`,
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      const data = response.data
+      console.log(data)
+      setGames(data)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getUserGames()
+  
+    
+  }, [])
+  
+  useEffect(() => {
+  console.log(games)
+  setGameLevels(selectedGame?.game_levels)
+    
+  }, [selectedGame])
+  
+
+
   return (
     <div className='whole-screen flex center'>
         <div className='progress-div'>
-          <ul className='games-list flex between'>
-            <li><NavLink to='asdadd'>Typing Invaders</NavLink></li>
-            <li><NavLink to='asdadd'>Shortcut King</NavLink></li>
-            <li><NavLink to='asdadd'>English</NavLink></li>
-            <li><NavLink to='/progress/'>French</NavLink></li>
+          <ul className='games-list flex '>
+            
+              {games.map((game)=>(
+                <li onClick={()=>{setClickedGame(game.game_name)
+                  setSelectedGame(game)
+                }} className={`${clickedGame===game.game_name?'active':''} `}>{game.game_name}</li>
+              ))}
+            
+
           </ul>
         <div className='game-container flex'>
-            <div className='mode flex column between'>
+            <div className='mode flex column around'>
                 <ul className='center flex column'>
-                  <li><NavLink to='/progress/'>Beginner</NavLink></li>
-                  <li><NavLink to='asdadd'>Intermediate</NavLink></li>
-                  <li><NavLink to='asdadd'>Advanced</NavLink></li>
+                  <li onClick={()=>setCLickedMode('BEGINNER')} className={`${clickedMode==='BEGINNER'?'active':''}`}>Beginner</li>
+                  <li onClick={()=>setCLickedMode('INTERMEDIATE')} className={`${clickedMode==='INTERMEDIATE'?'active':''}`}>Intermediate</li>
+                  <li onClick={()=>setCLickedMode('ADVANCED')} className={`${clickedMode==='ADVANCED'?'active':''}`}>Advanced</li>
                 </ul>
                 <ul className='center flex column'>
-                  <li><NavLink to='asdadd'>Daily challenges</NavLink></li>
-                  <li><NavLink to='asdadd'>Weekly challenges</NavLink></li>
-                  <li><NavLink to='asdadd'>Constest</NavLink></li>
+                  <li>Daily challenges</li>
+                  <li>Weekly challenges</li>
+                  <li>Constest</li>
                 </ul>
                 <ul className='center flex column'>
-                  <li><NavLink to='asdadd'>Infinite mode</NavLink></li>
-                  <li><NavLink to='asdadd'>Creator levels</NavLink></li>
-                  <li></li>
+                  <li>Infinite mode</li>
+                  <li>Creator levels</li>
+                  
                 </ul>
             
             </div>
             <div className='levels-div flex column align-center'>
-              <div className='stats-div flex between align-center border'>
-                 <h4 className='flex center'>wddsfs</h4> 
-                 <div className='border flex text-center center'>
-                    <h2>1adsaa sadf</h2>
-                 </div>
-                 <h4>wddsfs</h4> 
-                 <div className='border flex center text-center'>
-                 <h2>1adsaa  sadf</h2>
-                 </div>
-                 <h4>wddsfs</h4> 
-                 <div className='border flex center text-center'>
-                 <h2>1adsaa sadf </h2>
-                 </div>
-              </div>
-
               <div className='levels-container flex column align-center'>
-                <LevelCard gameName='Typing game' levelName='Level 1' levelId='1' status='COMPLETE' />
-                <LevelCard gameName='Typing game' levelName='Level 1' levelId='1' status='COMPLETE' />
-                <LevelCard gameName='Typing game' levelName='Level 1' levelId='1' status='COMPLETE' />
-                <LevelCard gameName='Typing game' levelName='Level 1' levelId='1' status='COMPLETE' />
-                <LevelCard gameName='Typing game' levelName='Level 1' levelId='1' status='COMPLETE' />
-                <LevelCard gameName='Typing game' levelName='Level 1' levelId='1' status='COMPLETE' />
+                {gameLevels?.filter(level => level.level_difficulty === clickedMode).
+                map((level) => (
+                  <LevelCard levelName={level.score} gameName={selectedGame.game_name}/>
+                ))}
               </div>
-
             </div>
             
         </div>
