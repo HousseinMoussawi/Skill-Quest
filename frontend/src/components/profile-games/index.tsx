d22d854f5286725af22209a241c60954fe9a24b2
Houssein Moussawi <am0149365@gmail.com>
2024-05-18 15:55:12 +0300

    frontend: created the logic for the the profile games component

--- frontend/src/components/profile-games/index.tsx
+++ frontend/src/components/profile-games/index.tsx
@@ -1,14 +1,48 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import './index.css'
+import axios from 'axios'
+import RewardCard from '../profile-reward-card'
+
+
 
 type Props = {
-    user:string,
+    
 }
 
-const ProfileGames:FC<Props> = ({user}) => {
-  return (
-    <div className={`profile-games flex between ${user=='PLAYER'?'column':''}`}>
+const ProfileGames:FC<Props> = ({}) => {
 
+  const [rewards,setRewards] = useState<any[]>([])
+  const token = localStorage.getItem('token')
+  const user = JSON.parse(localStorage.getItem('user') as string)
+
+   
+   const getAllRewards = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/users/${user._id}/rewards`,
+        {
+          headers:{
+            Authorization:`Bearer ${token}`,
+            'Content-Type':'application/json'
+          }
+        }
+      )
+      setRewards(response.data)
+   }
+     catch (error) {
+      console.log(error)
+    }
+  }
+
+   useEffect(() => {
+   getAllRewards()
+   }, [])
+   
+
+  return (
+    <div className='profile-games flex between'>
+      {rewards.map((reward) =>(
+        <RewardCard key={reward.reward_id}  imageURL={reward.reward_image_url} rewardName={reward.reward_name}/>
+      ))}
     </div>
   )
 }
