3f598c8c4c8ca67e78017879d79230effc692357
Houssein Moussawi <am0149365@gmail.com>
2024-05-18 16:09:58 +0300

    frontend: created the structure of the reward card component

--- frontend/src/components/reward-card/index.tsx
+++ frontend/src/components/reward-card/index.tsx
@@ -1,22 +1,37 @@
-import React, { FC } from 'react'
+import React, { FC, MouseEventHandler } from 'react'
 import './index.css'
-import Image from '../../assets/istockphoto-1333010525-612x612.jpg'
+import { useState } from 'react'
 
-type Props = {}
+type Props = {
+  imageURL:string,
+  rewardName: string,
+  price:number,
+  handleRewardPurchase: MouseEventHandler<HTMLButtonElement>,
+  getRewardId:(e:any)=>void,
+  rewardId:string,
+}
 
-const RewardCard: FC<Props> = () => {
+const RewardCard: FC<Props> = ({imageURL,price,rewardName,handleRewardPurchase,getRewardId,rewardId}) => {
+  const [hidden,setHidden] = useState<boolean>(true)
+
+
+  const handleHiddenToggle = (e:any)=>{
+    getRewardId(rewardId)
+    setHidden(!hidden)
+  }
+
   return (
-    <div className='reward-card flex column relative between'>
-        <img src={Image} alt="" />
+    <div className='reward-card flex column relative between' onClick={handleHiddenToggle}>
+        <img src={imageURL} alt="" />
         <div className='reward-card-info flex column between align-center'>
           <div className='reward-name flex between align-center'>
-              <h2>Game Name</h2>
-              <h4>Price</h4>
+              <h2>{rewardName}</h2>
+              <h4>{price}</h4>
           </div>
 
         </div>
-        <div className='buy-div flex hidden center absolute'>
-                <button>Buy</button>
+        <div className={`buy-div flex center absolute ${hidden?'hidden':''}`}  onClick={handleHiddenToggle}>
+                <button onClick={handleRewardPurchase}>Buy</button>
           </div>
     </div>
   )
